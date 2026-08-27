4b08423745f67c4db20ca532caa9bc0fb7edf5db
technovahub2025 <technovahubcareer@gmail.com>
2026-08-27 18:06:30 +0530

    fix: remove trailing slashes from allowed origins in CORS configuration

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "http://localhost:3001",
- " https://www.jothieyecare.com/",
- "https://jothieyecare.com/",
+ " https://www.jothieyecare.com",
+ "https://jothieyecare.com",
   "https://eyedonorfrontend.vercel.app",
   process.env.CLIENT_URL,
 ].filter(Boolean);
